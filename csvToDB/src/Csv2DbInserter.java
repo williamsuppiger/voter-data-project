917cf8334e7479da73e6c10265c693e7beedbaa8
William Suppiger <suppiger@usc.edu>
2020-11-27 19:16:56 -0800

    updated Csv inserter file (might not use)

---
+++ csvToDB/src/Csv2DbInserter.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.sql.*;
+
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ParseDouble;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.prefs.CsvPreference;
+
+public class Csv2DbInserter {
+	//inspired by: https://www.codejava.net/coding/java-code-example-to-insert-data-from-csv-to-database
+	
+	public static void main(String[] args) {
+
+	}
+
+	public static void csv
+
+}
